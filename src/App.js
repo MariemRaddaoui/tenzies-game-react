9a33996773de8eefd13b9fc652de1d6df025d220
MariemRaddaoui <96314948+MariemRaddaoui@users.noreply.github.com>
2023-02-07 12:26:23 +0100

    styled components - needs logic

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
 import './App.css';
+import Die from './components/Die';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+      <h1 className="title">Tenzies</h1>
+      <p className="instructions">Roll until all dice are the same. Click each die to freeze it at its current value between rolls.</p>
+      <div className="dice-container">
+          <Die number="1"/>
+          <Die number="1"/>
+          <Die number="1"/>
+          <Die number="1"/>
+          <Die number="1"/>
+          <Die number="1"/>
+          <Die number="1"/>
+          <Die number="1"/>
+          <Die number="1"/>
+          <Die number="1"/>
+      </div>
+      <button className="roll-dice" >Roll</button>
+    </main>
   );
 }
 
